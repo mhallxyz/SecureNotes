33420d3a9bbefce08a3926d1da75654122ca1667
Mathew Hall <m@mhall.xyz>
2018-05-15 21:54:49 +0100

    Async storage working

--- src/components/authentication.js
+++ src/components/authentication.js
@@ -15,31 +15,21 @@ export default class Authentication extends Component {
         pass1: "",
         pass2: "",
         same: false,
-        pw: null,
+        myKey: null,
       }
     }
 
-  async load(key) {
-    try {
-      const value = await AsyncStorage.getItem(key);
-      if (value !== null){
-        // We have data!!
-        console.log(value);
-        this.setState({pw: value})
-      }
-    } catch (error) {
-      // Error retrieving data
-    }
-  }
+
+  componentDidMount() {
+    AsyncStorage.getItem("myKey").then((value) => {
+        this.setState({"myKey": value});
+    }).done();
+  };
 
   save(key, value) {
-    try {
-      AsyncStorage.setItem(key, value)
-      .then(() => this.load("pwKey"))
-    } catch (error) {
-      // Error saving data
-    }
-  }
+    AsyncStorage.setItem("myKey", value);
+    this.setState({"myKey": value});
+  };
 
   render() {
     return(
@@ -50,10 +40,12 @@ export default class Authentication extends Component {
             <TextInput onChangeText={text => this.setState({pass1: text})} value={this.state.pass1} placeholder="Set a secure password" style={styles.textInput}/>
             <TextInput onChangeText={text => this.setState({pass2: text})} value={this.state.pass2} placeholder="Repeat the secure password" style={styles.textInput}/>
             <Text style={styles.text}>
-            Matching Passwords: {this.state.pass1.length > 0 && this.state.pass1 === this.state.pass2 ? "True" : "False"}
-            pw in state: {this.state.pw}
+              Matching Passwords: {this.state.pass1.length > 0 && this.state.pass1 === this.state.pass2 ? "True" : "False"}
             </Text>
-            <Button title="Create Password" style={styles.button} onPress={() => this.save("pwKey" ,this.state.pass1)}/>
+            <Text style={styles.text}>
+              myKey in state: {this.state.myKey}
+            </Text>
+            <Button title="Create Password" style={styles.button} onPress={() => this.save("myKey" ,this.state.pass1)}/>
         </View>
     )
   }
