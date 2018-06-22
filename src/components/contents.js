bba93e3789918102062f7c94de22d5d8dfc4ea84
Mathew Hall <m@mhall.xyz>
2018-06-22 21:22:16 +0100

    Multiple screens working

--- src/components/contents.js
+++ src/components/contents.js
@@ -9,6 +9,7 @@ import {
   TouchableOpacity
 } from 'react-native';
 import Entry from './entry';
+import { TextInput } from 'react-native-gesture-handler';
 var {height, width} = Dimensions.get('window');
 
 class Contents extends Component {
@@ -16,7 +17,8 @@ class Contents extends Component {
     super(props);
     this.state = {
       notes: ['fdghsfh','gdasfd','agdgfdga','gafadgf','fdghsfh','gdasfd','agdgfdga','gafadgf','fdghsfh','gdasfd','agdgfdga','gafadgf','fdghsfh','gdasfd','agdgfdga','gafadgf','fdghsfh','gdasfd','agdgfdga','gafadgf'],
-      addScreen: false
+      addScreen: false,
+      newNote: ""
     }
   }
 
@@ -27,26 +29,40 @@ class Contents extends Component {
   // };
 
   addNote(noteContent) {
-    const notes1 = this.state.notes
-    notes1.unshift(noteContent)
-    this.setState({notes: notes1})
+    if (this.state.addScreen) {
+    const notes1 = this.state.notes;
+    notes1.unshift(noteContent);
+    this.setState({
+      notes: notes1,
+      addScreen: false,
+      newNote: ''
+    });
+    } else {
+      this.setState({addScreen: true});
+    }
   }
 
   render() {
-    console.log(this.state.notes)
+    console.log(this.state.newNote)
     return( 
-    <View style={styles.notes}>
-      <ScrollView>
-        {this.state.notes.map((note, index) => <Entry key={index} note={note} />)}
-      </ScrollView>
-      <TouchableOpacity onPress={() => this.addNote("Button Works")} style={styles.button} title='Add Note'>
-      <View style={styles.buttonSurround}>
-        <Text style={styles.text}>
-          + Add Note
-        </Text>
+      <View style={styles.notes}>
+      {this.state.addScreen === true ? 
+      <View style={styles.noteEntryContainer}>
+        <Text style={styles.topText}>Add Note</Text>
+        <TextInput style={styles.noteEntryBox} placeholder="Enter new note here" value={this.state.newNote} onChangeText={text => this.setState({newNote: text})} />
+      </View> : null}
+      {this.state.addScreen === false ? 
+        <ScrollView>
+          {this.state.notes.map((note, index) => <Entry key={index} note={note} />)}
+        </ScrollView> : null}
+        <TouchableOpacity onPress={() => this.addNote(this.state.newNote)} style={styles.button} title='Add Note'>
+        <View style={styles.buttonSurround}>
+          <Text style={styles.text}>
+            + Add Note
+          </Text>
+        </View>
+        </TouchableOpacity>
       </View>
-      </TouchableOpacity>
-    </View>
     );
   }
 }
@@ -68,7 +84,26 @@ const styles = StyleSheet.create({
   text: {
     color: '#fff',
     fontSize: 20
+  },
+  topText: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  noteEntryBox: {
+    height: 75,
+    width: width,
+    position: 'relative',
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#000',
+    padding: 20
+  },
+  noteEntryContainer: {
+    padding: 12,
+    flex: 1
   }
 })
 
 export default Contents;
+
